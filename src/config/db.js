3f719953fc723b01ba80b320c49e341caa7634a3
Raphael Fruneaux <raphaelfruno@gmail.com>
2016-11-17 08:41:10 -0300

    Now settings supports environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@
 const pmongo = require('promised-mongo')
 const settings = {}
 
-settings.protocol = 'mongodb'
-settings.hostname = 'localhost'
-settings.port = '27017'
-settings.scheme = 'meusus'
+settings.protocol = process.env.GEOCODING_PROTOCOL || 'mongodb'
+settings.hostname = process.env.GEOCODING_HOSTNAME || 'localhost'
+settings.port = process.env.GEOCODING_PORT || '27017'
+settings.scheme = process.env.GEOCODING_SCHEME || 'meusus'
 settings.uri = `${settings.protocol}://${settings.hostname}:${settings.port}/${settings.scheme}`
 
 var db = pmongo(settings.uri);
